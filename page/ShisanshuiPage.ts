51545bf164217769907bab58f2be9a0cb8a6bf88
xiaoer <xiaoer@example.com>
2019-09-12 15:03:00 +0800

    client批量提交

--- page/ShisanshuiPage.ts
+++ page/ShisanshuiPage.ts
@@ -121,7 +121,7 @@ module gameshisanshui.page {
 			TongyongPageDef.ins.alertRecharge(StringU.substitute("老板，您的金币少于{0}哦~\n补充点金币去大杀四方吧~", limit), () => {
 				this._game.uiRoot.general.open(DatingPageDef.PAGE_CHONGZHI);
 			}, () => {
-			}, false, TongyongPageDef.TIPS_SKIN_STR['cz']);
+			}, true, TongyongPageDef.TIPS_SKIN_STR['cz']);
 		}
 
 		private chkPlayerIsGuest(): boolean {
